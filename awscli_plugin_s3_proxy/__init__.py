f0cdfd941423148063baadf548864aed7e0e8204
Sebastian Nowak <sebastian.nowak@rbinternational.com>
2018-07-19 10:19:14 +0200

    initial commit

---
+++ awscli_plugin_s3_proxy/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+PROXY = 'proxy'
+
+def get_proxy_from_profile(profile, command):
+    proxy = None
+    if command in profile:
+            if PROXY in profile[command]:
+                proxy = profile[command][PROXY]
+    return proxy
+
+def set_proxy_from_profile(parsed_args, **kwargs):
+    command = parsed_args.command
+    session = kwargs['session']
+    if parsed_args.profile:
+        session.set_config_variable('profile', parsed_args.profile)
+    service_proxy = get_proxy_from_profile(session.get_scoped_config(), command)
+    if service_proxy is not None:
+        print(f'Using S3 proxy: {service_proxy}')
+        os.environ["http_proxy"] = service_proxy
+        os.environ["https_proxy"] = service_proxy
+        os.environ["HTTP_PROXY"] = service_proxy
+        os.environ["HTTPS_PROXY"] = service_proxy
+        os.environ["NO_PROXY"] = "169.254.169.254"
+
+def awscli_initialize(cli):
+    cli.register('top-level-args-parsed', set_proxy_from_profile)
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup
+
+requires = ['awscli>=1.11.0']
+
+setup(
+    name='awscli-plugin-s3-proxy',
+    packages=['awscli_plugin_s3_proxy'],
+    version='0.1',
+    description='S3 proxy plugin for AWS CLI',
+    long_description=open('README.md').read(),
+    author='Sebastian Nowak',
+    author_email='snowak@bluesoft.net.pl',
+    url='https://github.com/nowak-ninja/awscli-plugin-s3-proxy',
+    download_url='https://github.com/nowak-ninja/awscli-plugin-s3-proxy/tarball/0.1',
+    keywords=['awscli', 'plugin', 's3', 'proxy'],
+    install_requires=requires,
+    classifiers = []
+)
